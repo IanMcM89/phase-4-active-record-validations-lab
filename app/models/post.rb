4cc119b62315d03cda3f9c37f9618e8e16866f01
Ian <ianmcm.89@gmail.com>
2022-04-28 18:14:48 -0400

    commit tree 2c3ef87f5f1f596edb4c1cca263ff4267ab97108

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validate :fiction_or_non_fiction, :true_facts_not_allowed
+
+  def fiction_or_non_fiction
+    array = ['Fiction', 'Non-Fiction']
+    errors.add(:category, "is not included in the list") if !array.include?(category)
+  end
+
+  def true_facts_not_allowed
+    errors.add(:title, "Title can't be True Facts") if title == "True Facts"
+  end
 end
